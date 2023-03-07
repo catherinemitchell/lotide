f518671042d07dc050bb5e6f7ca27625df9d4905
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-07 13:49:13 -0400

    Added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,14 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("\u2705" + "\u2705" + "\u2705" + " Assertion Passed: " + actual + " === " + expected);
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
 
   } else {
-    console.log("\u{1F6D1}" + "\u{1F6D1}" + "\u{1F6D1}" + " Assertion Failed: " + actual + "!== "  + expected);
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 2);
-
-
 
 
