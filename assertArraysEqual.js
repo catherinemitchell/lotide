0c8234951be7542fb13ed4d2b79afc8594913287
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-08 15:01:38 -0400

    created a function that compared two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+  const eqArrays = (arr1, arr2) => {
+    if (arr1.length === arr2.length) {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) return false;
+        }
+        return true;
+    }
+    return false;
+} 
+
+const assertArraysEqual = function (arr1, arr2) {
+
+if (eqArrays(arr1, arr2)) {
+  console.log("Assertion passed");
+} else {
+  console.log("Assertion failed");
+}
+
+}
+
+assertArraysEqual([1, 2, 3], [3, 4, 5]);
