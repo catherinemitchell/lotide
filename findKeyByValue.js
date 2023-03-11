11f857004409d36c96077c2bc732f6bd6ca67bc0
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-11 11:05:57 -0400

    iterated through an objet using Object.keys

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, value) {
+
+const genreOfShow = Object.keys(bestTVShowsByGenre);
+
+for (const genre of genreOfShow) {
+   if (bestTVShowsByGenre[genre] === value)
+
+return genre
+}
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
