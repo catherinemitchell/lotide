90aacc59afc9407d9e1d23a7422ead3f3c08afee
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 14:58:32 -0300

    created headTest file, imported head.js and moved the head.js file test into test/headTest.js

---
+++ test/headTest.js
@@ -0,0 +1,5 @@
+// const assertEqual = require('../assertEqual'); Do not need to import this because it is imported into the head file
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
