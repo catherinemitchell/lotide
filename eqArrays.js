73dcdbb62bcefd12b3eaf88c5bc237435d362568
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 15:54:28 -0300

    imported assertEqual and exported eqArrays.  Moved test code to eqArraysTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,15 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
 
-  } else {
-    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//   } else {
+//     console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const assertEqual = require('./assertEqual');
   
-  const eqArrays = (arr1, arr2) => {
+const eqArrays = (arr1, arr2) => {
     if (arr1.length === arr2.length) {
         for (let i = 0; i < arr1.length; i++) {
             if (arr1[i] !== arr2[i]) return false;
@@ -17,15 +19,9 @@ const assertEqual = function (actual, expected) {
     return false;
 } 
 
+module.exports = eqArrays;
 
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]))
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]))
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
 
 
