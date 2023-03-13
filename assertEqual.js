8eaa2c403d4b48f26855399da49ed30ee99c8e90
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 14:55:24 -0300

    moved test to the assertEqualTest file, and added export to assertEqual file

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,5 @@ const assertEqual = function(actual, expected) {
     console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs");
-assertEqual(1, 2);
 
-
+module.exports = assertEqual;
