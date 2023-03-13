90cd0d2bb3afeb0f403d69a6ad0cfb9d8d4b9994
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 16:20:54 -0300

    imported eqArrays and exported assertArraysEqual.  Moved test code into assertArraysEqualTest file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,23 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
+// const assertEqual = function (actual, expected) {
+//   if (actual === expected) {
+//     console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
 
-  } else {
-    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//   } else {
+//     console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
   
-  const eqArrays = (arr1, arr2) => {
-    if (arr1.length === arr2.length) {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) return false;
-        }
-        return true;
-    }
-    return false;
-} 
+//   const eqArrays = (arr1, arr2) => {
+//     if (arr1.length === arr2.length) {
+//         for (let i = 0; i < arr1.length; i++) {
+//             if (arr1[i] !== arr2[i]) return false;
+//         }
+//         return true;
+//     }
+//     return false;
+// } 
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function (arr1, arr2) {
 
@@ -27,4 +29,6 @@ if (eqArrays(arr1, arr2)) {
 
 }
 
-assertArraysEqual([1, 2, 3], [3, 4, 5]);
+module.exports = assertArraysEqual;
+
+
