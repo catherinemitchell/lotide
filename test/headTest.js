13cb8e21fe2626c6aaa59acaea98e364b814c631
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 16:37:27 -0300

    added the assertEqual require

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,5 @@
 // const assertEqual = require('../assertEqual'); Do not need to import this because it is imported into the head file
+const assertEqual = require('../assertEqual');
 const head = require('../head');
 
 assertEqual(head([5,6,7]), 5);
