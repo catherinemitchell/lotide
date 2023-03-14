1f031e2ba95f7af3286477a35bce5d4cf4df0ae1
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-14 07:27:13 -0300

    fixed linting issues

--- middle.js
+++ middle.js
@@ -1,18 +1,16 @@
-const assertArraysEqual = require("./assertArraysEqual");
-
 const middle = function(array) {
 
   const middleElements = [];
-  if(array.length <= 1) {
+  if (array.length <= 1) {
     return [];
   }
   if (array.length % 2 === 0) {
-  middleElements.push(array[Math.floor((array.length - 1) / 2)]);
-  } 
+    middleElements.push(array[Math.floor((array.length - 1) / 2)]);
+  }
   const midArray = (array[Math.floor(array.length / 2)]);
   middleElements.push(midArray);
   return middleElements;
-}
+};
 
 module.exports = middle;
 
