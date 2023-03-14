335661163b5f71fbe2d41d44619f80957deb61bc
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 22:42:52 -0300

    imported assertEqual

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2705\u2705\u2705 Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
