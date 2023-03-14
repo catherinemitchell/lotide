8d4cb8d7610ddd6e2616e86dcf94592853308640
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-14 07:11:27 -0300

    fixed linting issues

--- eqArrays.js
+++ eqArrays.js
@@ -6,18 +6,17 @@
 //     console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${actual} !== ${expected}`);
 //   }
 // };
-
-const assertEqual = require('./assertEqual');
+// const assertEqual = require('./assertEqual');
   
 const eqArrays = (arr1, arr2) => {
-    if (arr1.length === arr2.length) {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) return false;
-        }
-        return true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
     }
-    return false;
-} 
+    return true;
+  }
+  return false;
+};
 
 module.exports = eqArrays;
 
