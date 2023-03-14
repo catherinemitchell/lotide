70cef1bf124858f395e9026f3baa64656a866023
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 21:38:11 -0300

    added shorthand for exports

--- index.js
+++ index.js
@@ -3,36 +3,37 @@ const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
 const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKeyByValue = require('./findKeyByValue');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const without = require('./without');
-const takeUntil = require('./takeUntil');
-const findKey = require('./findKey');
-
-
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  without: without,
-  takeUntil: takeUntil,
-  findKey: findKey,
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
 
-};
+}; 
+  // assertObjectsEqual: assertObjectsEqual,
+  // countLetters: countLetters,
+  // countOnly: countOnly,
+  // eqObjects: eqObjects,
+  // findKeyByValue: findKeyByValue,
+  // letterPositions: letterPositions,
+  // map: map,
+  // without: without,
+  // takeUntil: takeUntil,
+  // findKey: findKey,
+
+
+  // const assertObjectsEqual = require('./assertObjectsEqual');
+// const countLetters = require('./countLetters');
+// const countOnly = require('./countOnly');
+// const eqObjects = require('./eqObjects');
+// const findKeyByValue = require('./findKeyByValue');
+// const letterPositions = require('./letterPositions');
+// const map = require('./map');
+// const without = require('./without');
+// const takeUntil = require('./takeUntil');
+// const findKey = require('./findKey');
+
