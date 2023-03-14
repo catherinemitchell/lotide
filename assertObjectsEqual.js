020c56d8fe7b6620f95b992fc9c6b1bd4a84d02e
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-14 07:05:12 -0300

    fixed linting issues

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,15 +1,14 @@
-const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 
-const assertObjectsEqual = function(actual, expected) { 
-  const inspect = require('util').inspect; 
-  if (eqObjects(actual, expected)) { 
-    console.log(`\u2705\u2705\u2705 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`); 
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`\u2705\u2705\u2705 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`); 
+    console.log(`\u{1F6D1}\u{1F6D1}\u{1F6D1} Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
-}
+};
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
-assertObjectsEqual(shirtObject, anotherShirtObject)
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject, anotherShirtObject);
