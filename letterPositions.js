cd31a5c918723ee81ad5579cae3b36a01c8392c4
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-13 22:58:34 -0300

    imported assertArraysEqual and eqArrays

--- letterPositions.js
+++ letterPositions.js
@@ -1,28 +1,11 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length === arr2.length) {
-      for (let i = 0; i < arr1.length; i++) {
-          if (arr1[i] !== arr2[i]) return false;
-      }
-      return true;
-  }
-  return false;
-} 
-
-const assertArraysEqual = function (arr1, arr2) {
-
-if (eqArrays(arr1, arr2)) {
-console.log("Assertion passed");
-} else {
-console.log("Assertion failed");
-}
-
-}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
 
   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] === ' ') continue; 
+    if (sentence[i] === ' ') continue;
     results[sentence[i]] = results[sentence[i]] ? results[sentence[i]].concat(i) : [i];
 
   }
