89c9167203bab0a2dd58f9d0c6968fd208e69ccb
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-14 07:25:28 -0300

    fixed linting issues

--- map.js
+++ map.js
@@ -1,19 +1,18 @@
-const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
 
-const results = [];
-for(let item of array) {
-  results.push(callback(item));
-}
-return results;
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
 };
 const firstCharacter = function(word) {
   return word[0];
-}
+};
 
 
 console.log(map(words, firstCharacter));
