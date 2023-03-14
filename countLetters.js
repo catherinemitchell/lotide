cfd4a42af5798a824cfd4e307b5fb55017d6a505
Catherine Mitchell <nicholscat@yahoo.com>
2023-03-14 07:08:24 -0300

    fixed linting issues

--- countLetters.js
+++ countLetters.js
@@ -1,19 +1,19 @@
-const assertEqual = require('./assertEqual');
+// const assertEqual = require('./assertEqual');
 
 const countLetters = function(string) {
- let letterCount = {};
+  let letterCount = {};
 
-for (let letter of string) {
-  if (letter === ' ') {
-    continue;
-  } 
+  for (let letter of string) {
+    if (letter === ' ') {
+      continue;
+    }
   
-if (letterCount[letter]) {
-letterCount[letter]++;
-} else {
-  letterCount[letter] = 1
-} 
-}
+    if (letterCount[letter]) {
+      letterCount[letter]++;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
   return letterCount;
-}
+};
 console.log(countLetters('hello world'));
